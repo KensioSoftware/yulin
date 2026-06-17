a2686c19e18e528a216a95cab0db4bf8b8046676
Hugh Grigg <hugh@kensiosoftware.co.uk>
2026-06-17 10:49:17 +0100

    docs: extract, compile and lint docs examples (#36)

---
+++ docs/services/cloudfront/serve-sim-cloudfront-localhost.example.ts
@@ -0,0 +1,71 @@
+/**
+ * Serving a simulated CloudFront Distribution on localhost.
+ */
+
+import { CreateDistributionCommand } from "@aws-sdk/client-cloudfront";
+import { CreateBucketCommand, PutObjectCommand } from "@aws-sdk/client-s3";
+
+import { SimAws } from "@kensio/yulin";
+import { serveSimAws } from "@kensio/yulin/serve";
+
+const simAws = new SimAws();
+const srv = await serveSimAws({ simAws });
+
+try {
+  const simS3 = simAws.s3();
+  const simCloudFront = simAws.cloudFront();
+
+  await simS3.createBucket(
+    new CreateBucketCommand({
+      Bucket: "foo-bucket",
+    }),
+  );
+
+  await simS3.putObject(
+    new PutObjectCommand({
+      Bucket: "foo-bucket",
+      Key: "hello.txt",
+      Body: "Hello from simulated CloudFront",
+    }),
+  );
+
+  const createDistributionOutput = await simCloudFront.createDistribution(
+    new CreateDistributionCommand({
+      DistributionConfig: {
+        CallerReference: "localhost-assets-cdn",
+        Comment: "Localhost Assets CDN",
+        Enabled: true,
+        Origins: {
+          Quantity: 1,
+          Items: [
+            {
+              Id: "assets-origin",
+              DomainName: "foo-bucket.s3.amazonaws.com",
+              S3OriginConfig: {
+                OriginAccessIdentity: "",
+              },
+            },
+          ],
+        },
+        DefaultCacheBehavior: {
+          TargetOriginId: "assets-origin",
+          ViewerProtocolPolicy: "allow-all",
+        },
+      },
+    }),
+  );
+
+  const distroHostname = createDistributionOutput.Distribution?.DomainName;
+
+  if (distroHostname === undefined) {
+    throw new Error("Expected sim CloudFront Distribution hostname");
+  }
+
+  const url = srv.localUrl(`http://${distroHostname}/hello.txt`);
+  const response = await fetch(url);
+
+  console.log(response.status);
+  console.log(await response.text());
+} finally {
+  srv.close();
+}
---
+++ docs/services/cloudfront/sim-cloudfront-function.example.ts
@@ -0,0 +1,108 @@
+/**
+ * Simulated CloudFront Functions.
+ */
+
+import {
+  CreateDistributionCommand,
+  CreateFunctionCommand,
+} from "@aws-sdk/client-cloudfront";
+import { CreateBucketCommand } from "@aws-sdk/client-s3";
+
+import { SimAws } from "@kensio/yulin";
+import {
+  makeCffFunctionCodeInput,
+  type CloudFrontFunction,
+} from "@kensio/yulin/cloudfront";
+import { serveSimAws } from "@kensio/yulin/serve";
+
+const simAws = new SimAws();
+const srv = await serveSimAws({ simAws });
+
+try {
+  const simS3 = simAws.s3();
+  const simCloudFront = simAws.cloudFront();
+
+  await simS3.createBucket(
+    new CreateBucketCommand({
+      Bucket: "foo-bucket",
+    }),
+  );
+
+  function viewerRequestFunction(
+    event: CloudFrontFunction.ViewerRequestEvent,
+  ): CloudFrontFunction.Request | CloudFrontFunction.Response {
+    if (event.request.uri === "/old-page.html") {
+      return {
+        statusCode: 302,
+        statusDescription: "Found",
+        headers: {
+          location: {
+            value: "https://example.test/new-page.html",
+          },
+        },
+      };
+    }
+
+    return event.request;
+  }
+
+  const createFunctionOutput = await simCloudFront.createFunction(
+    new CreateFunctionCommand({
+      Name: "redirect-old-page",
+      FunctionConfig: {
+        Comment: "Redirect old page",
+        Runtime: "cloudfront-js-2.0",
+      },
+      FunctionCode: makeCffFunctionCodeInput(viewerRequestFunction),
+    }),
+  );
+
+  const createDistributionOutput = await simCloudFront.createDistribution(
+    new CreateDistributionCommand({
+      DistributionConfig: {
+        CallerReference: "function-cdn",
+        Comment: "Function CDN",
+        Enabled: true,
+        Origins: {
+          Quantity: 1,
+          Items: [
+            {
+              Id: "assets-origin",
+              DomainName: "foo-bucket.s3.amazonaws.com",
+              S3OriginConfig: {
+                OriginAccessIdentity: "",
+              },
+            },
+          ],
+        },
+        DefaultCacheBehavior: {
+          TargetOriginId: "assets-origin",
+          ViewerProtocolPolicy: "allow-all",
+          FunctionAssociations: {
+            Quantity: 1,
+            Items: [
+              {
+                EventType: "viewer-request",
+                FunctionARN: createFunctionOutput.FunctionMetadata.FunctionARN,
+              },
+            ],
+          },
+        },
+      },
+    }),
+  );
+
+  const distroHostname = createDistributionOutput.Distribution?.DomainName;
+
+  if (distroHostname === undefined) {
+    throw new Error("Expected sim CloudFront Distribution hostname");
+  }
+
+  const url = srv.localUrl(`http://${distroHostname}/old-page.html`);
+  const response = await fetch(url, { redirect: "manual" });
+
+  console.log(response.status);
+  console.log(response.headers.get("location"));
+} finally {
+  srv.close();
+}
