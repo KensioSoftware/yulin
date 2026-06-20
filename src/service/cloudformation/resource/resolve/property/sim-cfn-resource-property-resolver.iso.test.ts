9839944a17284a166f48fbd0fe9800f91e376649
Hugh Grigg <hugh@kensiosoftware.co.uk>
2026-06-20 14:43:35 +0100

    test: remove redundant empty test

--- src/service/cloudformation/resource/resolve/property/sim-cfn-resource-property-resolver.iso.test.ts
+++
@@ -1,7 +0,0 @@
-import { describe, it } from "vitest";
-
-describe("Sim CFN resource property resolver", () => {
-  it("leaves an unresolved Ref dependency in place for the next pass", () => {
-    //
-  });
-});
