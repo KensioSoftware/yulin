920d745154c1f77cee99142e57672f5ad92ba854
Hugh Grigg <hugh@kensiosoftware.co.uk>
2026-06-20 10:06:28 +0100

    fix: replace lazy `unknown` in NonDeterministicBackgroundTasks.complete

--- src/util/type-guard/record.ts
+++ src/util/type-guard/record.ts
@@ -1,6 +1,7 @@
 /**
  * Check if a given value is a Record<string, unknown>
  */
+// eslint-disable-next-line no-restricted-syntax
 export function isRecord(value: unknown): value is Record<string, unknown> {
   return typeof value === "object" && value !== null && !Array.isArray(value);
 }
