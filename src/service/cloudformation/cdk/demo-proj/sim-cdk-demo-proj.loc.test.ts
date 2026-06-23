a08ffa86fd8a4eb262b74471defa3e36913bd5fd
Hugh Grigg <hugh@kensiosoftware.co.uk>
2026-06-23 22:09:09 +0100

    test: demo project local integration test

---
+++ src/service/cloudformation/cdk/demo-proj/sim-cdk-demo-proj.loc.test.ts
@@ -0,0 +1,188 @@
+import { afterAll, beforeAll, describe, it } from "vitest";
+
+/**
+ * Slower local integration test. Calls the real CDK CLI to synth the output
+ * template file and asset manifest to pass to sim CloudFormation.
+ */
+import { SimAws } from "../../../aws/sim-aws.js";
+import { SimAwsLocalServer } from "../../../../serve/index.js";
+import { TempDir } from "../../../../util/filesystem/temp-dir.js";
+import { TestCdkProject } from "../../../../util/filesystem/test-cdk-project.js";
+import path from "node:path";
+import {
+  assertIdentical,
+  assertStringLength,
+  assertStringStartsWith,
+} from "@kensio/smartass";
+
+describe("Sim CDK BucketDeployment local integration", () => {
+  const simAws = new SimAws();
+
+  const srv: SimAwsLocalServer = new SimAwsLocalServer({ simAws });
+
+  beforeAll(async () => {
+    await srv.listen();
+  });
+
+  afterAll(() => {
+    srv.close();
+  });
+
+  it("deploys test demo project into sim AWS", async () => {
+    // Given a small test demo project.
+    const projectDir = new TempDir();
+    await projectDir.writeFile("public/index.html", "<h1>Root</h1>");
+    await projectDir.writeFile("public/foo/index.html", "<h1>Foo</h1>");
+
+    // Including a CFF.
+    await projectDir.writeFile(
+      "cff/rewrite-function.js",
+      `
+function handler(event) {
+  if (event.request.uri === "/redirect-me.html") {
+    return {
+      statusCode: 302,
+      statusDescription: "Found",
+      headers: {
+        location: { value: "https://example.test/from-embedded-source.html" },
+      },
+    };
+  }
+  
+  if (event.request.uri.endsWith("/")) {
+    event.request.uri += "index.html";
+  }
+
+  return event.request;
+}
+`,
+    );
+
+    const handlerFile = projectDir.join("cff/rewrite-function.js");
+
+    // And a CDK stack to deploy the test demo project.
+    const publicDir = projectDir.join("public");
+    const cdkProject = new TestCdkProject({ projectDir });
+    await cdkProject.writeCdkAppFile(
+      `
+import * as cdk from "aws-cdk-lib/core";
+import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
+import * as origins from "aws-cdk-lib/aws-cloudfront-origins";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as s3deploy from "aws-cdk-lib/aws-s3-deployment";
+
+const app = new cdk.App();
+const stack = new cdk.Stack(app, "TestStack", {
+  env: { account: "111111111111", region: "eu-west-2" },
+});
+
+const siteBucket = new s3.Bucket(stack, "SiteBucket", {
+  blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+  encryption: s3.BucketEncryption.S3_MANAGED,
+  enforceSSL: true,
+  versioned: true,
+  removalPolicy: cdk.RemovalPolicy.RETAIN,
+  autoDeleteObjects: false,
+});
+
+const redirectFunction = new cloudfront.Function(
+  stack,
+  "RedirectFunction",
+  {
+    code: cloudfront.FunctionCode.fromFile({
+      filePath: ${JSON.stringify(handlerFile)},
+    }),
+  },
+);
+
+const distribution = new cloudfront.Distribution(stack, "SiteDistribution", {
+  defaultRootObject: "index.html",
+  defaultBehavior: {
+    origin: origins.S3BucketOrigin.withOriginAccessControl(siteBucket),
+    viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+    allowedMethods: cloudfront.AllowedMethods.ALLOW_GET_HEAD_OPTIONS,
+    cachedMethods: cloudfront.CachedMethods.CACHE_GET_HEAD_OPTIONS,
+    compress: true,
+    functionAssociations: [
+      {
+        function: redirectFunction,
+        eventType: cloudfront.FunctionEventType.VIEWER_REQUEST,
+      },
+    ],
+  },
+  errorResponses: [
+    {
+      httpStatus: 403,
+      responseHttpStatus: 404,
+      responsePagePath: "/404.html",
+      ttl: cdk.Duration.minutes(5),
+    },
+    {
+      httpStatus: 404,
+      responseHttpStatus: 404,
+      responsePagePath: "/404.html",
+      ttl: cdk.Duration.minutes(5),
+    },
+  ],
+});
+
+new s3deploy.BucketDeployment(stack, "DeploySite", {
+  sources: [s3deploy.Source.asset(${JSON.stringify(publicDir)})],
+  destinationBucket: siteBucket,
+  distribution,
+  distributionPaths: ["/*"],
+  prune: true,
+});
+
+new cdk.CfnOutput(stack, "SiteBucketName", {
+  value: siteBucket.bucketName,
+});
+
+new cdk.CfnOutput(stack, "CloudFrontDistributionId", {
+  value: distribution.distributionId,
+});
+
+new cdk.CfnOutput(stack, "DistributionDomainName", {
+  value: distribution.distributionDomainName,
+});
+
+app.synth();
+      `,
+    );
+
+    // And we synth the CDK template.
+    const cdkOutDir = await cdkProject.synth();
+
+    // When we deploy the template into sim CloudFormation.
+    const simCfn = simAws.cloudFormation();
+    const stack = await simCfn.deployTemplateFile(
+      path.join(cdkOutDir, "TestStack.template.json"),
+    );
+
+    // Then the Outputs should be available.
+    assertStringStartsWith(
+      stack.outputs.get("SiteBucketName")?.value,
+      "sitebucket",
+    );
+    const distroId = stack.outputs.get("CloudFrontDistributionId")?.value;
+    assertStringLength(distroId, 14);
+    const distroSubdomain = stack.outputs.get("DistributionDomainName")?.value;
+    assertIdentical(
+      distroSubdomain,
+      `${distroId.toLowerCase()}.cloudfront.net`,
+    );
+
+    // And we should be able to interact with the test demo project.
+    const redirectedRes = await fetch(
+      `http://${distroSubdomain}.sim-aws.localhost:${srv.port}/redirect-me.html`,
+      { redirect: "manual" },
+    );
+    assertIdentical(redirectedRes.status, 302);
+
+    const fooRes = await fetch(
+      `http://${distroSubdomain}.sim-aws.localhost:${srv.port}/foo/`,
+    );
+    assertIdentical(fooRes.status, 200);
+    assertIdentical(await fooRes.text(), "<h1>Foo</h1>");
+  });
+});
